79f56cd9207691801ff7becfefb11e1e292556b6
Arteaga Esteban <Esteban.Arteaga@scania.com>
2026-01-31 13:50:53 -0300

    Update Entrega Ufen parte2

--- PROYECTO_FINANZAS_R/01_ENTREGAS/Parte 2/app.R
+++ PROYECTO_FINANZAS_R/01_ENTREGAS/Parte 2/app.R
@@ -3,7 +3,7 @@
 # - Se eliminó la visual "Carpeta de guardado fijo"
 # - Impresión multipágina: imprime SOLO la cotización en ventana limpia
 
-OUTPUT_DIR <- "C:/Users/sclear/OneDrive/FEN CIENCIA DATOS/RSTUDIO/UFEN/PROYECTO_FINANZAS_R/02_APP_CALCULADORA"
+OUTPUT_DIR <- "C:/Users/sclear/OneDrive/FEN CIENCIA DATOS/RSTUDIO/UFEN/PROYECTO_FINANZAS_R/01_ENTREGAS"
 
 # ---------------- Paquetes ----------------
 pkgs <- c("shiny","bslib","tidyverse","glue","lubridate","readr","htmltools","base64enc")
@@ -33,7 +33,7 @@ if (!file.exists(readme_path)) {
       "",
       "Ejecuta en la consola (cambia la ruta según donde lo ejecutes):",
       "",
-      "setwd(\"C:/Users/sclear/OneDrive/FEN CIENCIA DATOS/RSTUDIO/UFEN/PROYECTO_FINANZAS_R/02_APP_CALCULADORA\")",
+      "setwd(\"C:/Users/sclear/OneDrive/FEN CIENCIA DATOS/RSTUDIO/UFEN/PROYECTO_FINANZAS_R/01_ENTREGAS\")",
       "shiny::runApp(launch.browser = TRUE)",
       "",
       "Luego: completa los inputs y usa la pestaña 'Cotización (Imprimible)'.",
